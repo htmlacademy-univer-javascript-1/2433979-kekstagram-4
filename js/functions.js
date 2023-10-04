2d71914cec06d233f342c0ea043effc391c9f238
Евгения Фролова <nadyafrolova59@gmail.com>
2023-10-04 19:44:06 +0500

    создает функции

---
+++ js/functions.js
@@ -0,0 +1,9 @@
+//Функция для проверки длины строки.
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
+
+//Функция для проверки, является ли строка палиндромом.
+const isPalindrome = (string) => {
+  const normalizedString = string.replaceAll().toLowerCase();
+  const reverseString = normalizedString.split().reverse().join();
+  return (normalizedString === reverseString);
+};
