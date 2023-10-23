105ddade85aa43110f110c83e2f6c5fc0da66026
Евгения Фролова <nadyafrolova59@gmail.com>
2023-10-23 19:10:13 +0500

    исправила константы

--- js/main.js
+++ js/main.js
@@ -1,4 +1,34 @@
 const SIMILAR_OBJECT_COUNT = 25;
+const AVATAR_MIN_COUNT = 1;
+const AVATAR_MAX_COUNT = 6;
+const LIKE_MIN_COUNT = 15;
+const LIKE_MAX_COUNT = 200;
+const COMMENT_MIN_COUNT = 0;
+const COMMENT_MAX_COUNT = 30;
+const MESSAGE_MIN_COUNT = 1;
+const MESSAGE_MAX_COUNT = 2;
+const MESSAGES = [
+  'Всё отлично', 'В целом всё неплохо.', 'Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.',
+  'В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают.',
+  'Как можно было поймать такой неудачный момент?!'
+];
+const NAMES = [
+  'Алена',
+  'Максим',
+  'Коля',
+  'Женя',
+  'Вася',
+  'Лена',
+  'Ксюша',
+  'Антон',
+  'Катя',
+  'Илья'
+];
+
 //функция для получения случайного числа из диапазона
 const getRandomInteger = (min, max) => {
   const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
@@ -18,7 +48,7 @@ const getRndIntWithoutRepeat = (min, max) => {
       return null;
     }
     while(previousValues.includes(currentValue)){
-      let currentValue = getRandomInteger(min, max);
+      currentValue = getRandomInteger(min, max);
     }
     previousValues.push(currentValue);
     return currentValue;
@@ -26,43 +56,38 @@ const getRndIntWithoutRepeat = (min, max) => {
 };
 
 //функция для получения случайного элемента массива
-const getRandomArrayElement = (elements) => {
-  return (elements[getRandomInteger(0, elements.length - 1)])
-};
+const getRandomArrayElement = (elements) => (
+  (elements[getRandomInteger(0, elements.length - 1)])
+);
 const descriptions = ['Как дела дорогие подписчики?', 'Всем хорошего дня!', 'Я на пляже в Дубае'];
 
 //формирует текст комментария посредством взятия одного или двух случайных предложений из представленных в messages
 const createMessage = () => {
-  const messages = ['Всё отлично', 'В целом всё неплохо.', 'Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.','В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.','Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.','Лица у людей на фотке перекошены, как будто их избивают.', 'Как можно было поймать такой неудачный момент?!'];
-  const indexMessage = getRndIntWithoutRepeat(0, messages.length - 1);
-  return getRandomInteger(1,2) === 1 ? messages[indexMessage()] : messages[indexMessage()] + ' ' + messages[indexMessage()]
-}
+  const indexMessage = getRndIntWithoutRepeat(0, MESSAGES.length - 1);
+  return getRandomInteger(MESSAGE_MIN_COUNT,MESSAGE_MAX_COUNT) === 1 ? MESSAGES[indexMessage()] : MESSAGES[indexMessage()] + ' ' + MESSAGES[indexMessage()]
+};
 
+const idComment = getRndIntWithoutRepeat(COMMENT_MIN_COUNT, COMMENT_MAX_COUNT);
 //создает комментарий
-const createComment = () => {
-  const names = ['Алена', 'Максим', 'Коля', 'Женя', 'Вася', 'Лена', 'Ксюша', 'Антон', 'Катя', 'Илья'];
-  const idComment = getRndIntWithoutRepeat(0, 30);
-  return {
-    id: idComment(),//любое число. Идентификаторы не должны повторяться,
-    avatar: `img/avatar-${getRandomInteger(1,6)}.svg`, //это строка, значение которой формируется по правилу img/avatar-{{случайное число от 1 до 6}}.svg,
-    message: createMessage(),//вам необходимо взять одно или два случайных предложения из представленных ниже,
-    name: getRandomArrayElement(names)//Имена авторов также должны быть случайными. Подставляйте случайное имя в поле,
-  };
-};
+const createComment = () => ({
+  id: idComment(),
+  avatar: `img/avatar-${getRandomInteger(AVATAR_MIN_COUNT,AVATAR_MAX_COUNT)}.svg`,
+  message: createMessage(),
+  name: getRandomArrayElement(NAMES)
+});
 
-const similarComments = Array.from({length: getRandomInteger(0,30)}, createComment);
-const createObject = () => {
-  const idObject = getRndIntWithoutRepeat(1, SIMILAR_OBJECT_COUNT);
-  const urlObject = getRndIntWithoutRepeat(1, SIMILAR_OBJECT_COUNT);
-  return {
-    id: idObject(), //идентификатор опубликованной фотографии. Это число от 1 до 25. Идентификаторы не должны повторяться
-    url: `photos/${urlObject()}.jpg`, //{{i}} — это число от 1 до 25. Адреса картинок не должны повторяться.
-    description: getRandomArrayElement(descriptions), //строка — описание фотографии. Описание придумайте самостоятельно
-    likes: getRandomInteger(15, 200), //Случайное число от 15 до 200.
-    comments: similarComments /*массив объектов — список комментариев, оставленных другими пользователями к этой фотографии.
-     Количество комментариев к каждой фотографии — случайное число от 0 до 30.
-     Все комментарии генерируются случайным образом.*/
-  };
-};
+const idObject = getRndIntWithoutRepeat(1, SIMILAR_OBJECT_COUNT);
+const urlObject = getRndIntWithoutRepeat(1, SIMILAR_OBJECT_COUNT);
+const createObject = () => ({
+  id: idObject(),
+  url: `photos/${urlObject()}.jpg`,
+  description: getRandomArrayElement(descriptions),
+  likes: getRandomInteger(LIKE_MIN_COUNT, LIKE_MAX_COUNT),
+  comments: Array.from({length: getRandomInteger(0,30)}, createComment)
+});
+
 
 const similarObjects = Array.from({length: SIMILAR_OBJECT_COUNT}, createObject);
+
+console.log(similarObjects);
+
