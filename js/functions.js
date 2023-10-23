d16e66dda48c74c4a53d14b24bb1fb79b374e861
Евгения Фролова <nadyafrolova59@gmail.com>
2023-10-23 17:36:25 +0500

    исправила function.js

--- js/functions.js
+++ js/functions.js
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 683d5674b8ec1b932bab6a22a39edb87dade6295
 //Функция для проверки длины строки.
 const checkStringLength = (string, maxLength) => string.length <= maxLength;
 
 //Функция для проверки, является ли строка палиндромом.
 const isPalindrome = (string) => {
-<<<<<<< HEAD
   const normalizedString = string.replaceAll(' ','').toLowerCase();
   const reverseString = normalizedString.split('').reverse().join('');
-  return (normalizedString === reverseString);
-}
-=======
-  const normalizedString = string.replaceAll().toLowerCase();
-  const reverseString = normalizedString.split().reverse().join();
   return normalizedString === reverseString;
-};
->>>>>>> 683d5674b8ec1b932bab6a22a39edb87dade6295
+}
+
