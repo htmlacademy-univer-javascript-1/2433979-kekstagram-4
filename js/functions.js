66fd75532d4317becf78cd704f14c42641679c9e
Евгения Фролова <nadyafrolova59@gmail.com>
2023-10-31 13:20:40 +0500

    исправила написание констант

--- js/functions.js
+++ js/functions.js
@@ -2,21 +2,21 @@ const checkTimeMeeting = (startWorkingDay, endWorkingDay, startMeeting,
   durationMeeting) => {
   //нужно проверить, что начало встречи находится в промежутке
   //потом проверить, что время начала + продолжительность < конца дня
-  const startWorkHour = Number(startWorkingDay.split(':')[0]);
-  const startWorkMin = Number(startWorkingDay.split(':')[1]);
-  const endWorkHour = Number(endWorkingDay.split(':')[0]);
-  const endWorkMin = Number(endWorkingDay.split(':')[1]);
-  const startMeetHour = Number(startMeeting.split(':')[0]);
-  const startMeetMin = Number(startMeeting.split(':')[1]);
-  const meetingHour = durationMeeting / 60;
-  const meetingMin = durationMeeting % 60;
-  const endMeetingHour = startMeetHour + meetingHour;
-  const endMeetingMin = startMeetMin + meetingMin;
-  if ((startWorkHour < startMeetHour) ||
-  (startWorkHour === startMeetHour && startWorkMin <= startMeetMin)){
-    if ((endWorkHour > startMeetHour) ||
-    (endWorkHour === startMeetHour && endWorkMin >= startMeetMin)){
-      if ((endWorkHour > endMeetingHour) || (endWorkHour === endMeetingHour && endWorkMin >= endMeetingMin)){
+  const START_WORK_HOUR = Number(startWorkingDay.split(':')[0]);
+  const STERT_WORK_MIN = Number(startWorkingDay.split(':')[1]);
+  const END_WORK_HOUR = Number(endWorkingDay.split(':')[0]);
+  const END_WORK_MIN = Number(endWorkingDay.split(':')[1]);
+  const START_MEET_HOUR = Number(startMeeting.split(':')[0]);
+  const STERT_MEET_MIN = Number(startMeeting.split(':')[1]);
+  const MEETING_HOUR = durationMeeting / 60;
+  const MEETING_MIN = durationMeeting % 60;
+  const END_MEETING_HOUR = START_MEET_HOUR + MEETING_HOUR;
+  const END_MEETING_MIN = STERT_MEET_MIN + MEETING_MIN;
+  if ((START_WORK_HOUR < START_MEET_HOUR) ||
+  (START_WORK_HOUR === START_MEET_HOUR && STERT_WORK_MIN <= STERT_MEET_MIN)){
+    if ((END_WORK_HOUR > START_MEET_HOUR) ||
+    (END_WORK_HOUR === START_MEET_HOUR && END_WORK_MIN >= STERT_MEET_MIN)){
+      if ((END_WORK_HOUR > END_MEETING_HOUR) || (END_WORK_HOUR === END_MEETING_HOUR && END_WORK_MIN >= END_MEETING_MIN)){
         return true;
       }
     }
