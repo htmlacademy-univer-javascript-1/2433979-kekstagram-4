51fe8b1cae665e5f6d9e0fcdcd100b24a5a21928
Евгения Фролова <nadyafrolova59@gmail.com>
2023-10-31 13:11:45 +0500

    вернула в первоначальный вид

--- js/functions.js
+++ js/functions.js
@@ -1,25 +1,9 @@
-const checkTimeMeeting = (startWorkingDay, endWorkingDay, startMeeting,
-  durationMeeting) => {
-  //нужно проверить, что начало встречи находится в промежутке
-  //потом проверить, что время начала + продолжительность < конца дня
-  const startWorkHour = Number(startWorkingDay.split(':')[0]);
-  const startWorkMin = Number(startWorkingDay.split(':')[1]);
-  const endWorkHour = Number(endWorkingDay.split(':')[0]);
-  const endWorkMin = Number(endWorkingDay.split(':')[1]);
-  const startMeetHour = Number(startMeeting.split(':')[0]);
-  const startMeetMin = Number(startMeeting.split(':')[1]);
-  const meetingHour = durationMeeting / 60;
-  const meetingMin = durationMeeting % 60;
-  const endMeetingHour = startMeetHour + meetingHour;
-  const endMeetingMin = startMeetMin + meetingMin;
-  if ((startWorkHour < startMeetHour) ||
-  (startWorkHour === startMeetHour && startWorkMin <= startMeetMin)){
-    if ((endWorkHour > startMeetHour) ||
-    (endWorkHour === startMeetHour && endWorkMin >= startMeetMin)){
-      if ((endWorkHour > endMeetingHour) || (endWorkHour === endMeetingHour && endWorkMin >= endMeetingMin)){
-        return true;
-      }
-    }
-  }
-  return false;
+//Функция для проверки длины строки.
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
+
+//Функция для проверки, является ли строка палиндромом.
+const isPalindrome = (string) => {
+  const normalizedString = string.replaceAll(' ','').toLowerCase();
+  const reverseString = normalizedString.split('').reverse().join('');
+  return normalizedString === reverseString;
 };
