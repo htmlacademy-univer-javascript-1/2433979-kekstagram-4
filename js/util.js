b90160d247a2fc50f072b6b7cbde5ff394cda2e1
Евгения Фролова <nadyafrolova59@gmail.com>
2023-12-12 20:37:10 +0500

    добавила функцию проверки нажата ли escape

--- js/util.js
+++ js/util.js
@@ -29,4 +29,6 @@ const getRandomArrayElement = (elements) => (
   (elements[getRandomInteger(0, elements.length - 1)])
 );
 
-export {getRandomInteger, getRndIntWithoutRepeat, getRandomArrayElement};
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+export {getRandomInteger, getRndIntWithoutRepeat, getRandomArrayElement, isEscapeKey};
